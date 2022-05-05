1566006948c3ee7f8df62f113bd8e74a9808c037
Semicolon1647 <Semicolon1647@proton.me>
2022-05-04 21:52:18 -0700

    oapi: Migrate process_payments to use the API

--- ordering-api/orders/management/commands/process_payments.py
+++ ordering-api/orders/management/commands/process_payments.py
@@ -1,11 +1,11 @@
-from datetime import datetime
-from monero.backends.jsonrpc.wallet import JSONRPCWallet
-from monero.wallet import Wallet
-
 import logging
+from datetime import datetime
+from urllib.parse import urljoin
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from orders.models import Order
+from requests import Session, RequestException
 
 
 logger = logging.getLogger(__name__)
@@ -15,51 +15,42 @@ class Command(BaseCommand):
     help = 'Processes incoming monero transactions.'
 
     def handle(self, *_, **__):
-        host=settings.MONERO_RPC_HOST
-        port=settings.MONERO_RPC_PORT
-        user=settings.MONERO_RPC_USERNAME
-        password=settings.MONERO_RPC_PASSWORD
-
-        self.stderr.write(
-            f"Processing incoming payments using RPC {user}@{host}:{port}..."
-        )
-        wallet = Wallet(host=host, port=port, user=user, password=password)
+        base_url = settings.CAS_BASE_URL
+        self.stdout.write(f"base_url: {base_url}")
 
         orders = Order.objects.filter(state=Order.State.CREATED)
-        self.stderr.write(f"{orders.count()} orders to process")
+        self.stdout.write(f"to_process: {orders.count()}")
 
-        for order in orders:
-            total_price = order.total_price()
+        with Session() as s:
+            for order in orders:
+                expected = order.total_price()
 
-            self.stderr.write(f"Order #{order.pk} expected XMR{total_price}")
-            self.stderr.write(f"  Address {order.address}")
+                self.stdout.write(f" - order: #{order.pk}")
+                self.stdout.write(f"   expected: {expected}")
+                self.stdout.write(f"   address: {order.address}")
 
-            incoming_payments = wallet.incoming(
-                local_address=order.address,
-                max_height=settings.MONERO_TXN_MAX_HEIGHT,
-                confirmed=True,
-            )
-            total_received = sum((p.amount for p in incoming_payments))
-            self.stderr.write(
-                f"  Got XMR{total_received} over {len(incoming_payments)} payments"
-            )
+                try:
+                    response = s.get(
+                        urljoin(base_url, f'/api/addresses/{order.address}')
+                    )
+                except RequestException as e:
+                    self.stderr.write(repr(e))
+                    continue
 
-            if total_received < total_price:
-                self.stderr.write(f"  Did not receive enough payment, skipping")
-                break
-            
-            # Use last transactio as the source of information
-            txn = incoming_payments[-1]
+                data = response.json()
+                received = data['total_xmr']
+                txn_hash = data['txn_hash']
 
-            self.stderr.write(
-                f"  Received enough payment, marking it as purchased"
-            )
-            order.mark_paid(
-                txn_hash=txn.hash,
-                date=datetime.now()
-            )
-            order.full_clean()
-            order.save()
+                self.stdout.write(f"  received: {received}")
+                self.stdout.write(f"  txn_hash: {txn_hash}")
+                if received < expected:
+                    self.stdout.write(f"   paid: no")
+                    break
 
-        self.stderr.write("Done.")
+                self.stdout.write(f"  paid: yes")
+                order.mark_paid(txn_hash=txn_hash, date=datetime.now())
+                order.full_clean()
+                order.save()
+
+            self.stderr.write("Done")
 
